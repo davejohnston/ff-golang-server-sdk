a4233de2bb2068b9048303487e04e79b90b8df29
simonharness <163144759+simonharness@users.noreply.github.com>
2024-05-29 18:35:45 +0100

    Fix for the following issue: Trying to open a file provided as an input in a variable. The content of this variable might be controlled by an attacker who could change it to hold unauthorised file paths from the system.  In this way, it is possible to exfiltrate confidential information or such.

--- examples/tls/example.go
+++ examples/tls/example.go
@@ -90,7 +90,7 @@ func getEnvOrDefault(key, defaultStr string) string {
 func loadCertificates(filePaths []string) (*x509.CertPool, error) {
 	pool := x509.NewCertPool()
 	for _, ca := range filePaths {
-		caBytes, err := os.ReadFile(ca)
+		caBytes, err := os.ReadFile(filepath.Clean(ca))
 		if err != nil {
 			return nil, fmt.Errorf("failed to read CA certificate from file: %w", err)
 		}
